6369f16fde886d36b98cb8787d9e7e2b2ca30b5d
Jonathan Bijl <sannoayo@gmail.com>
2023-07-14 09:26:24 -0400

    Accessing URL Parameters

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
-import './App.css';
-import Navbar from "./components/Navbar";      
-import HomePage from "./pages/HomePage";       
-import AboutPage from "./pages/AboutPage";    
-import ProjectsPage from "./pages/ProjectsPage";   
-import ErrorPage from "./pages/ErrorPage";         
- 
+import "./App.css";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ErrorPage from "./pages/ErrorPage";
+import HomePageWithNavigate from "./pages/HomePageWithNavigate";
 import { Routes, Route } from "react-router-dom";
-import HomePageWithNavigate from "./pages/HomePageWithNavigate";  // <== IMPORT 
-import projectsData from "./projects-data.json";
- 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navbar />
- 
-//       <Routes>
-//         {/* <Route  path="/" element={<HomePage />} /> */}
-//         <Route  path="/" element={<HomePageWithNavigate />} />   {/* <== ADD */}
-//         <Route path="/about" element={<AboutPage/>} />
-//         <Route path="/projects" element={<ProjectsPage/>} />
- 
-//         <Route path="*" element={<ErrorPage />} />
-//       </Routes>      
-//     </div>
-//   );
-// }
- 
-// export default App;
+
+import projectsData from './projects-data.json';
+
+import ProjectDetailsPage from "./pages/ProjectDetailsPage";  // <== IMPORT
 
 function App() {
   return (
@@ -34,19 +17,26 @@ function App() {
       <Navbar />
       
       <Routes>
-        {/* <Route  path="/" element={<HomePage />} /> */}
-        <Route  path="/" element={<HomePageWithNavigate />} />
-        <Route path="/about" element={<AboutPage/>} />
+        {/* <Route  path="/" element={ <HomePage /> } /> */}
+
+        <Route path="/" element={ <HomePageWithNavigate /> } />
+        <Route path="/about" element={ <AboutPage /> } />
         
-        <Route 
-          path="/projects" 
-          element={ <ProjectsPage projects={projectsData} /> } 
+        <Route
+          path="/projects"
+          element={ <ProjectsPage projects={projectsData} /> }
         />
- 
-        <Route path="*" element={<ErrorPage />} />
+        
+        {/*  ADD  */}
+        <Route 
+          path="/projects/:projectId" 
+          element={ <ProjectDetailsPage /> } 
+        />        
+
+        <Route path="*" element={ <ErrorPage /> } />
       </Routes>
     </div>
   );
 }
- 
+
 export default App;
